fce6f5742af0a858eca69dea08366c27f2f6b7a9
Anne Kwok <guomulian@gmail.com>
2020-07-09 12:10:03 -0400

    Clean up notebooks, utils formatting.

--- cleaning/utils.py
+++ cleaning/utils.py
@@ -14,13 +14,13 @@ def print_cols_with_na_values(df):
 
 
 def remove_constant_cols(df):
-    '''Returns the given dataframe with the constant columns removed.'''
+    """Returns the given dataframe with the constant columns removed."""
     return df.loc[:, (df != df.iloc[0]).any()]
 
 
 def handle_datetime_types(df):
-    '''Attempts to automatically convert datetime columns to the correct type.
-    '''
+    """Attempts to automatically convert datetime columns to the correct type.
+    """
     for col in df.columns:
         if df[col].dtype == 'object':
             try:
@@ -33,10 +33,10 @@ def handle_datetime_types(df):
 
 
 def is_constant_col(df, group_by=None, dropna=True):
-    '''Returns a list of columns with constant value across all rows.
+    """Returns a list of columns with constant value across all rows.
        group_by : Group the DataFrame by group_by first.
        dropna   : Don't include NaN in the counts.
-    '''
+    """
     if group_by:
         return [col for col in df.columns if col != group_by and
                 all(nunique == 1
@@ -48,12 +48,11 @@ def is_constant_col(df, group_by=None, dropna=True):
 
 
 def squish_dataframe(df, group_by, dropna=True):
-    '''"Aggregates by list for columns with more than one distinct value.
+    """Aggregates by list for columns with more than one distinct value.
         For constant columns it simply takes that value.
-    '''
+    """
     constant_cols = is_constant_col(df, group_by, dropna)
     aggfuncs = {col: (lambda values: values.mode()) if col in constant_cols
                 else list for col in df.columns if col != group_by}
 
     return df.groupby(group_by).agg(aggfuncs).reset_index()
-
