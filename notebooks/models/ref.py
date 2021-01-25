8682676b297dfcabf205680992fcf9565ade93e2
Anne Kwok <anne_kwok@dfci.harvard.edu>
2021-01-25 12:07:19 -0500

    Add version of summary adjusted for GOC.

--- notebooks/models/ref.py
+++ notebooks/models/ref.py
@@ -4,3 +4,12 @@ FORMULA_CONTROL = "C(SEX, Treatment(reference='M')) + "\
     "ADMISSION_AGE + "\
     "ELIX_SCORE + "\
     "SOFA"
+
+
+FORMULA_CONTROL_WITH_GOC = "C(SEX, Treatment(reference='M')) + "\
+    "C(MARITAL_STATUS, Treatment(reference='MARRIED')) + "\
+    "C(ETHNICITY, Treatment(reference='(NON-HISPANIC) WHITE')) + "\
+    "ADMISSION_AGE + "\
+    "ELIX_SCORE + "\
+    "SOFA + "\
+    "IDENTIFIED_CONV_GOC"
